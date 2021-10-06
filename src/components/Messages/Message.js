5062c754bbfe956b903064c51febfc2dd13725be
sumit298 <sumitsinhassss@outlook.com>
2021-10-06 23:46:00 +0530

    added react-player modified youtube video player

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 import './Message.scss'
 import { Avatar } from '@material-ui/core'
 import ReactLinkify from 'react-linkify'
+import ReactPlayer from 'react-player'
 
 const isOwnUser = (message, user) =>
   message.user.id === user.uid ? 'message__self' : ''
@@ -10,6 +11,46 @@ const isOwnUser = (message, user) =>
 const timeFromNow = (timestamp) => moment(timestamp).calendar()
 
 export default function Message({ message, user }) {
+  // const [playing, setPlaying] = React.useState(true);
+  // const videoRef = React.useRef(null);
+  // const handlePlayPause = ()=>{
+  //   setPlaying(!playing)
+  // }
+  // const handlePlay = ()=>{
+  //   console.log('onPlay');
+  //   setPlaying(true)
+  // }
+  // const handlePause = ()=>{
+  //   console.log('onPause');
+  //   setPlaying(false)
+  // }
+  // React.useEffect(()=>{
+  //   window.addEventListener('scroll', (handlePlayPause))
+  //   return()=>{
+  //     window.removeEventListener('scroll', handlePlayPause);
+  //   }
+  // },[])
+  // const startVideo = ()=>{
+  //   videoRef.current.pause();
+  //   setPlaying(false);
+  // }
+  // const pauseVideo = ()=>{
+  //   videoRef.current.play();
+  //   setPlaying(true);
+  // }
+  // const handleScroll = ()=>{
+  //   if(playing){
+  //     pauseVideo();
+  //   }
+  // }
+  // const handleVideoPress = ()=>{
+  //   if(playing){
+  //     startVideo()
+  //   }
+  //   else{
+  //     pauseVideo();
+  //   }
+  // }
   const componentDecorator = (href, text, key) => (
     <a
       style={{ fontWeight: 'bold' }}
@@ -38,6 +79,25 @@ export default function Message({ message, user }) {
   const isImage = (message) =>
     message.hasOwnProperty('image') && !message.hasOwnProperty('content')
 
+  const ReactPlayerYoutube = () => {
+    return (
+      <ReactPlayer
+        url={`//www.youtube.com/embed/${videoId}`}
+        config={{
+          youtube: {
+            playerVars: { showinfo: 1 },
+          },
+        }}
+        playing={true}
+        light={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+       
+        playsinline={true}
+        // onPause={}
+        // ref={videoRef}
+        
+      />
+    )
+  }
   const contentRendered = isImage(message) ? (
     <img
       src={message.image}
@@ -47,20 +107,21 @@ export default function Message({ message, user }) {
     />
   ) : checkURL(message.content) ? (
     <img
-      className={"content__image"}
+      className={'content__image'}
       src={message.content}
       alt={message.content}
     />
   ) : videoId ? (
-    <iframe
-      width="700"
-      height="400"
-      frameBorder="0"
-      allowFullScreen={true}
-      src={`//www.youtube.com/embed/${videoId}`}
-      title="youtube video"
-    ></iframe>
+    <ReactPlayerYoutube />
   ) : (
+    // <iframe
+    //   width="700"
+    //   height="400"
+    //   frameBorder="0"
+    //   allowFullScreen={true}
+    //   src={`//www.youtube.com/embed/${videoId}`}
+    //   title="youtube video"
+    // ></iframe>
     <p className="content">{message.content}</p>
   )
 
